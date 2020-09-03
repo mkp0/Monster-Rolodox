7922ac5cc49b1b643caa7f1e15ddb65d433eb0e7
InnO <63262620+mkp042@users.noreply.github.com>
2020-09-03 17:18:59 +0530

    Updates

---
+++ precache-manifest.d9fb46ff5a5943c4c1b628659d37562e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80bf17e30e5f50a11253afb19c3c67b8",
+    "url": "/Monster-Rolodox/index.html"
+  },
+  {
+    "revision": "f054f382fcb9f435d580",
+    "url": "/Monster-Rolodox/static/css/main.035ed275.chunk.css"
+  },
+  {
+    "revision": "4a43f021320c20e275a9",
+    "url": "/Monster-Rolodox/static/js/2.fcf6991a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodox/static/js/2.fcf6991a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f054f382fcb9f435d580",
+    "url": "/Monster-Rolodox/static/js/main.bbd05a98.chunk.js"
+  },
+  {
+    "revision": "4fe87c35a04cce400ee5",
+    "url": "/Monster-Rolodox/static/js/runtime-main.48baad65.js"
+  }
+]);
